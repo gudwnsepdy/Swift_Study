bd1bd00977c7ab1e2663de530fb2aab0f5e51cb4
Hyoungjoo Lee <gudwnsepdy@naver.com>
2021-05-25 00:40:03 +0900

    add: update 5.24

---
+++ MyDatePicker/MyDatePicker/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  MyDatePicker
+//
+//  Created by 형주 on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd hh:mm:ss"
+        return formatter
+    }()
+    
+    @IBAction func didDatePickerValueChanged(_ sender: UIDatePicker) {
+        print("value changed")
+        
+        let date: Date = self.datePicker.date
+        let dateString: String = self.dateFormatter.string(from: date)
+        
+        self.dateLabel.text = dateString
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.datePicker.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)
+    }
+
+
+}
+
---
+++ ViewTransition/ViewTransition/UserInformation.swift
@@ -0,0 +1,15 @@
+//
+//  UserInformation.swift
+//  ViewTransition
+//
+//  Created by 형주 on 2021/05/24.
+//
+
+import Foundation
+
+class UserInformation {
+    static let shared: UserInformation = UserInformation()
+    
+    var name: String?
+    var age: String?
+}
